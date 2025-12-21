8a56ba8de1bb68cc50de546686722f7339a639da
GonzaloBJ <gonzalobarahonaj@gmail.com>
2025-12-21 16:23:48 -0300

    fix script generador de templates para generar ruta en templatesData

--- WebAPI/scripts/CVTemplateDirStructGenerator.py
+++ WebAPI/scripts/CVTemplateDirStructGenerator.py
@@ -1,18 +1,21 @@
+import json
 import os
 from pathlib import Path
 import sys
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from config import TEMPLATES_SOURCE
 
-def generate(name: str):
+def generate(name: str, description: str):
     try:
+        color_schemes_path = "colorSchemes"
         # Definimos la base: templates/nombre_del_template
         base_path = Path("templates") / name
         
         # Definimos las rutas de los archivos
         archivos = [
-            base_path / "colorSchemes" / "lightBlue.css",
-            base_path / "colorSchemes" / "darkRed.css",
+            base_path / color_schemes_path / "lightBlue.css",
+            base_path / color_schemes_path / "darkRed.css",
             base_path / f"{name}.css",
             base_path / f"{name}.html"
         ]
@@ -24,8 +27,29 @@ def generate(name: str):
             # Crea el archivo vacío
             ruta_archivo.touch()
             print(f"Creado: {ruta_archivo}")
+        
+        templates_data_source = Path(TEMPLATES_SOURCE)
+        if not templates_data_source.exists():
+            templates_data_source.touch()
+        
+        nuevo_registro_template = {
+            "name": name,
+            "rootPath": fr"./{base_path}/",
+            "colorSchemesPath": fr"{color_schemes_path}/",
+            "description": description
+        }
+
+        with open(templates_data_source, 'r', encoding='utf-8') as templates_data:
+            cv_templates = json.load(templates_data)
+
+        cv_templates.append(nuevo_registro_template)
+
+        with open(templates_data_source, 'w', encoding='utf-8') as updated_templates_data:
+            json.dump(cv_templates, updated_templates_data, indent=4)
+        
+        print(f"Actualizado: {TEMPLATES_SOURCE}")
     except Exception as e:
         print(f"Ocurrió un error al generar las rutas: {e}")
 
 # Ejecutar la función para generar las rutas        
-generate("githubBasicResume")
+generate("githubBasicResume", "Pensado para usar en Github.")
